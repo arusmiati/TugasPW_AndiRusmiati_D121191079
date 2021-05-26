f6e5886e389525539d69d5fd4e1e87189b35af77
arusmiati <mhiaaa31@gmail.com>
2021-05-26 14:51:39 +0800

    Pertemuan 10 (Insert Data)

--- ProjectWeb/Pertemuan 10/functions.php
+++ ProjectWeb/Pertemuan 10/functions.php
@@ -24,4 +24,22 @@ function query($query)
 
     return $rows;
 }
+
+function Tambah($data){
+    $conn = koneksi();
+
+    $Gambar = htmlspecialchars($data['Gambar']);
+    $Nama = htmlspecialchars($data['Nama']);
+    $Nim = htmlspecialchars($data['Nim']);
+    $Email = htmlspecialchars($data['Email']);
+    $Departemen = htmlspecialchars($data['Departemen']);
+    
+    $query = "INSERT INTO mahasiswa VALUES (null, '$Gambar', '$Nama', '$Nim', '$Email', '$Departemen');";
+
+    mysqli_query($conn, $query);
+    
+    echo mysqli_error($conn);
+    return mysqli_affected_rows($conn);
+
+}
 ?>
---
+++ ProjectWeb/Pertemuan 10/tambah.php
@@ -0,0 +1,70 @@
+<?php
+
+require 'functions.php';
+
+if (isset($_POST['Tambah'])) {
+    if (Tambah($_POST) > 0) {
+        echo "<script> 
+                    alert('data berhasil ditambahkan'); 
+                    document.location.href = 'latihan3.php';
+               </script>";
+    } else {
+        echo "data gagal ditambahkan";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h3>Form Tambah Data Mahasiswa</h3>
+    <form action="" method="POST">
+        <ul>
+            <li>
+                <label>
+                    Gambar :
+                    <input type="text" name="Gambar" required>
+                </label>
+            </li>
+            <li>
+                <label>
+                    Nama :
+                    <input type="text" name="Nama" autofocus required>
+                </label>
+            </li>
+            <li>
+                <label>
+                    Nim :
+                    <input type="text" name="Nim" required>
+                </label>
+            </li>
+            <li>
+                <label>
+                    Email :
+                    <input type="text" name="Email" required>
+                </label>
+            </li>
+            <li>
+                <label>
+                    Departemen :
+                    <input type="text" name="Departemen" required>
+                </label>
+            </li>
+
+            <li>
+                <button type="submit" name="Tambah">Tambah Data</button>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
