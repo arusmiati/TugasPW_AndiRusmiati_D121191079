4af685251262cccb7e16bce7931e96a2a2946f41
arusmiati <mhiaaa31@gmail.com>
2021-05-27 11:30:59 +0800

    Add Pertemuan 11 (Insert & Update Data)

---
+++ ProjectWeb/Pertemuan 11/functions.php
@@ -0,0 +1,72 @@
+<?php 
+
+function koneksi()
+{
+    //koneksi ke database dan pilih database
+    return mysqli_connect('localhost', 'root', '', 'web');
+}
+
+function query($query)
+{
+    $conn = koneksi();
+
+    $result = mysqli_query($conn, $query);
+
+    // //jika hasilnya ada 1 data
+    if(mysqli_num_rows($result)==1){
+        return mysqli_fetch_assoc($result);
+    }
+
+    $rows = []; 
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
+
+function Tambah($data){
+    $conn = koneksi();
+
+    $Gambar = htmlspecialchars($data['Gambar']);
+    $Nama = htmlspecialchars($data['Nama']);
+    $Nim = htmlspecialchars($data['Nim']);
+    $Email = htmlspecialchars($data['Email']);
+    $Departemen = htmlspecialchars($data['Departemen']);
+    
+    $query = "INSERT INTO mahasiswa VALUES (null, '$Gambar', '$Nama', '$Nim', '$Email', '$Departemen');";
+
+    mysqli_query($conn, $query) or die(mysqli_error($conn));
+    return mysqli_affected_rows($conn);
+
+}
+
+function Hapus($Id){
+    $conn = koneksi();
+
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE Id = $Id") or die(mysqli_error($conn));
+    return mysqli_affected_rows($conn);
+}
+
+function Ubah($data){
+    $conn = koneksi();
+
+    $Id = ($data['Id']);
+    $Gambar = htmlspecialchars($data['Gambar']);
+    $Nama = htmlspecialchars($data['Nama']);
+    $Nim = htmlspecialchars($data['Nim']);
+    $Email = htmlspecialchars($data['Email']);
+    $Departemen = htmlspecialchars($data['Departemen']);
+
+    $query = "UPDATE mahasiswa SET
+                Gambar = '$Gambar',
+                Nama = '$Nama',
+                Nim = '$Nim',
+                Email = '$Email',
+                Departemen = '$Departemen'
+              WHERE Id = $Id";
+
+    mysqli_query($conn, $query) or die(mysqli_error($conn));
+    return mysqli_affected_rows($conn);
+}
+?>
---
+++ ProjectWeb/Pertemuan 11/ubah.php
@@ -0,0 +1,75 @@
+<?php
+
+require 'functions.php';
+
+$Id = $_GET['Id'];
+
+$m = query("SELECT * FROM mahasiswa WHERE Id = $Id");
+
+if (isset($_POST['Ubah'])) {
+    if (Ubah($_POST) > 0) {
+        echo "<script>
+            alert('Data berhasil diubah');
+            document.location.href = 'index.php';
+        </script>";
+    } else {
+        echo 'Data gagal diubah';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h3>Form Ubah Data Mahasiswa</h3>
+    <form action="" method="POST">
+        <input type="hidden" name="Id" required value="<?= $m['Id']; ?>">
+        <ul>
+            <li>
+                <label>
+                    Gambar :
+                    <input type="text" name="Gambar" required value="<?= $m['Gambar']; ?>">
+                </label>
+            </li>
+            <li>
+                <label>
+                    Nama :
+                    <input type="text" name="Nama" autofocus required value="<?= $m['Nama']; ?>">
+                </label>
+            </li>
+            <li>
+                <label>
+                    Nim :
+                    <input type="text" name="Nim" required value="<?= $m['Nim']; ?>">
+                </label>
+            </li>
+            <li>
+                <label>
+                    Email :
+                    <input type="text" name="Email" required value="<?= $m['Email']; ?>">
+                </label>
+            </li>
+            <li>
+                <label>
+                    Departemen :
+                    <input type="text" name="Departemen" required value="<?= $m['Departemen']; ?>">
+                </label>
+            </li>
+
+            <li>
+                <button type="submit" name="Ubah">Ubah Data</button>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
