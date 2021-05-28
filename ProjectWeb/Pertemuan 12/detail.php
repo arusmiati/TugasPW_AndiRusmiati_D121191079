f16a945a8627a4e4e5b495fd7fa0305e31c39769
arusmiati <mhiaaa31@gmail.com>
2021-05-28 11:52:04 +0800

    Add Pertemuan 12 (Login)

---
+++ ProjectWeb/Pertemuan 12/detail.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'functions.php';
+
+if(!isset($_GET['Id'])){
+    header("Location: index.php");
+    exit;
+}
+//ambil id dari URL
+$Id = $_GET['Id'];
+
+//query mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE Id = $Id");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h3>Detail Mahasiswa</h3>
+    <ul>
+        <li><img src="img/<?= $m['Gambar']; ?>" width="60px"></li>
+        <li>Nama : <?= $m['Nama']; ?></li>
+        <li>Nim : <?= $m['Nim']; ?></li>
+        <li>Email : <?= $m['Email']; ?></li>
+        <li>Departemen : <?= $m['Departemen']; ?></li>
+        <li><a href="ubah.php?Id=<?= $m['Id']; ?>">Ubah</a> | <a href="hapus.php?Id=<?= $m['Id']; ?>"
+        onclick="return confirm('Apakah Anda Yakin?');">Hapus</a></li>
+        <li><a href="index.php">Kembali ke Daftar Mahasiswa</a></li>
+    </ul>
+</body>
+
+</html>
---
+++ ProjectWeb/Pertemuan 12/hapus.php
@@ -0,0 +1,15 @@
+<?php 
+
+require 'functions.php';
+
+$Id = $_GET['Id'];
+
+if(Hapus($Id) > 0){
+    echo "<script>
+        alert('Data berhasil dihapus');
+        document.location.href = 'index.php';
+        </script>";
+}else{
+    echo 'Data gagal dihapus';
+}
+?>
